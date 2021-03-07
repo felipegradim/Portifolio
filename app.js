ebe2c9896edff8399ed104a74f930c64b8e885ed
felipegradim <79435241+felipegradim@users.noreply.github.com>
2021-03-07 15:20:29 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+const hi = document.querySelector("#hi");
+const webdev = document.querySelector("#webdev");
+const picture = document.querySelector("#picture");
+const letter1 = document.querySelector("#letter1");
+const letter2 = document.querySelector("#letter2");
+const letter3 = document.querySelector("#letter3");
+const letter4 = document.querySelector("#letter4");
+const navbar = document.querySelector("#navbar");
+
+
+const tl = new TimelineMax();
+
+
+tl.fromTo(hi, 3, {x:"-200%"}, {x:"0%" ,ease:Power2.easeInOut})
+tl.fromTo(webdev, 1.5, {x:"200%"}, {x:"0%" ,ease:"elastic"},1)
+tl.fromTo(picture, 3, {y:"-100%"}, {y:"0%" ,ease:Bounce.easeOut},2)
+tl.fromTo(letter1, 1.5, {opacity:"0"}, {opacity:"1" ,ease:Power2.easeInOut},2)
+tl.fromTo(letter2, 1.5, {opacity:"0"}, {opacity:"1" ,ease:Power2.easeInOut},2)
+tl.fromTo(letter3, 1.5, {opacity:"0"}, {opacity:"1" ,ease:Power2.easeInOut},2)
+tl.fromTo(letter4, 1.5, {blinking:"0"}, {blinking:"10" ,ease:Power2.easeInOut},2)
+tl.fromTo(navbar, 1, {opacity:"0"}, {opacity:"1" ,ease:Power2.easeInOut},2)
+
